9e59f5c076b6b226f5c2101132e4a6fa978d147d
vksinghh25 <vipkumar@expedia.com>
2019-09-14 14:39:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Main.js';
+import './styles/stylesheet.css';
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
