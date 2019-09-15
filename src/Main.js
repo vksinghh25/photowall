d92e415f7a30dbf4abfebac7c9b35e2819913d16
Vipan K Singh <vksinghh@Vipans-MacBook-Pro.local>
2019-09-15 18:31:23 +0530

    Add Photo Functionality.

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Title from './Title.js';
 import PhotoWall from './PhotoWall.js';
+import AddPhoto from './AddPhoto.js';
 
 class Main extends Component {
   constructor() {
+    console.log("contructor");
     super();
     this.state = {
-      posts: [{
-          id: "0",
-          description: "beautiful landscape",
-          imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-          "3919321_1443393332_n.jpg"
-        }, {
-          id: "1",
-          description: "Aliens???",
-          imageLink: "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
-          "08323785_735653395_n.jpg"
-       }, {
-         id: "2",
-         description: "On a vacation!",
-         imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-       }]
+      posts: [],
+      screen: 'photos'    // photos, addPhotos
     }
     this.removePhoto = this.removePhoto.bind(this);
+    this.navigate = this.navigate.bind(this);
   }
 
   removePhoto(postRemoved) {
@@ -30,14 +20,63 @@ class Main extends Component {
     this.setState({posts: this.state.posts.filter(post => post !== postRemoved)});
   }
 
+  componentDidMount() {
+    console.log("componentDidMount");
+    const data = this.simulateFetchPosts();
+    this.setState({
+      posts: data
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  navigate() {
+    this.setState({
+      screen: 'addPhotos'
+    });
+  }
+
   render() {
-    console.log("Rendering");
+    console.log("render");
     return(
       <div>
-        <Title/>
-        <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto}/>
+        {
+          this.state.screen === 'photos' && (
+            <div>
+              <Title/>
+              <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/>
+            </div>
+          )
+        }
+        {
+          this.state.screen === 'addPhotos' && (
+            <div>
+              <AddPhoto/>
+            </div>
+          )
+        }
       </div>
     )
+  }
+
+  simulateFetchPosts() {
+    return [{
+        id: "0",
+        description: "beautiful landscape",
+        imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+        "3919321_1443393332_n.jpg"
+      }, {
+        id: "1",
+        description: "Aliens???",
+        imageLink: "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
+        "08323785_735653395_n.jpg"
+     }, {
+       id: "2",
+       description: "On a vacation!",
+       imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+     }];
   }
 }
 
--- src/PhotoWall.js
+++ src/PhotoWall.js
@@ -3,8 +3,11 @@ import Photo from './Photo.js';
 
 class PhotoWall extends Component {
   render() {
-    return <div className="photoGrid">
+    return <div>
+            <a onClick={this.props.onNavigate} className="addIcon" href="#addPhotos"/>
+            <div className="photoGrid">
             {this.props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={this.props.onRemovePhoto}/>)}
+            </div>
           </div>
   }
 }
